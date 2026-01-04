6d501ad669f4fa0dc2a40dfc03667d6e238159b0
WorldEdit2 <79908900+WorldEdit2@users.noreply.github.com>
2026-01-04 23:08:55 +0300

    Update api_service.py

--- api_service.py
+++ api_service.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv(
 
 "DATABASE_URL",
 
-"postgresql://volkan:201eAcwsVd7cf1jyxQL2F1wn4VZ3FSbW@dpg-d3t90iur433s73b5q070-a.oregon-postgres.render.com/cloud_db_gwjr"
+"postgresql://enes:soHlUE2WMgKtb3l26C6j0JxbP5Hn2hkL@dpg-d5dcgaili9vc73dfrqi0-a.oregon-postgres.render.com/enes"
 
 )
 
